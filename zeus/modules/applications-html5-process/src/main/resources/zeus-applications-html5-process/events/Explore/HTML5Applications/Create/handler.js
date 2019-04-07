a0ec523eab42ac32271a40c3e5efe1f72eb2c4f4
ThuF <jordan.p.pavlov@gmail.com>
2019-04-07 10:03:38 +0300

    Updates content

--- zeus/modules/applications-html5-process/src/main/resources/zeus-applications-html5-process/events/Explore/HTML5Applications/Create/handler.js
+++ zeus/modules/applications-html5-process/src/main/resources/zeus-applications-html5-process/events/Explore/HTML5Applications/Create/handler.js
@@ -1,16 +1,22 @@
-var dao = require('zeus-applications-html5/data/dao/Explore/HTML5Applications');
-var BuildsDao = require('zeus-build/data/dao/Build/Builds');
+var uuid = require("utils/v4/uuid")
+var dao = require("zeus-applications-html5/data/dao/Explore/HTML5Applications");
+var BuildsDao = require("zeus-build/data/dao/Build/Builds");
+var ServiceAccountsDao = require("zeus-build/data/dao/Deliver/ServiceAccounts");
 
 exports.onMessage = function(message) {
 	var event = JSON.parse(message);
 	var application = dao.get(event.key.value);
+
+	// TODO Get the service account in more inteligent way!
+	var serviceAccount = ServiceAccountsDao.list()[0];
+
 	BuildsDao.create({
+		Name: serviceAccount.Name + "-build-" + uuid.random().substring(0, 8),
 		Image: "promartio/helium",
-		GitUrl: application.GitUrl,
-		GitRevision: application.GitRevision,
-		ServiceAccount: 2
+		GitUrl: "https://github.com/dirigiblelabs/zeus-v3-build-template-html5",
+		GitRevision: "master",
+		ServiceAccount: serviceAccount.Id
 	});
-	console.log("HTML5 Application Created: " + JSON.stringify(application ));
 };
 
 exports.onError = function(error) {
